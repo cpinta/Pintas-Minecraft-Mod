557b254d0deb7aa65bf54d4d6cca4025026ca796
Pinta <pinta.christian@outlook.com>
2024-05-07 18:56:53 -0500

    added shooting snowballs when someone is on top of a bingus

--- src/main/java/com/cpinta/pintamod/blocks/BingusBlock.java
+++ src/main/java/com/cpinta/pintamod/blocks/BingusBlock.java
@@ -2,6 +2,7 @@ package com.cpinta.pintamod.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
 import net.minecraft.network.chat.ChatMessageContent;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.OutgoingPlayerChatMessage;
@@ -11,12 +12,21 @@ import net.minecraft.stats.Stats;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ExperienceOrb;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.LargeFireball;
+import net.minecraft.world.entity.projectile.Snowball;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
@@ -35,6 +45,7 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -72,33 +83,16 @@ public class BingusBlock extends DirectionalBlock {
         }
 
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
-        List<String> items = Arrays.asList("item.minecraft.cocoa_beans", "item.minecraft.cookie");
-        if(items.contains(itemstack.getItem().getDescriptionId().toString())){
-            PlayerChatMessage chatMessage = PlayerChatMessage.system(new ChatMessageContent("its cocoa beans ong"));
-            pPlayer.createCommandSourceStack().sendChatMessage(OutgoingPlayerChatMessage.create(chatMessage), true, ChatType.bind(ChatType.CHAT, pPlayer));
+        List<String> items = Arrays.asList(
+                "item.minecraft.cocoa_beans",
+                "item.minecraft.cookie");
 
-            pLevel.setBlockAndUpdate(pPos, pState.setValue(EATING, Integer.valueOf(1)));
-            if(stateDefinition.any().getValue(EATING) == 0){
-                stateDefinition.any().setValue(EATING, 1);
+        if(items.contains(itemstack.getItem().getDescriptionId().toString())){
+            if(pState.getValue(EATING) == 0){
+                pLevel.setBlockAndUpdate(pPos, pState.setValue(EATING, Integer.valueOf(1)));
+                itemstack.shrink(1);
             }
         }
-//        if(itemstack.getItem() instanceof ItemCocoaBlock){
-//
-//        } else if () {
-//
-//        }
-//        else{
-//
-//        }
-//        if (optional.isPresent()) {
-//            if (!pLevel.isClientSide && campfireblockentity.placeFood(pPlayer, pPlayer.getAbilities().instabuild ? itemstack.copy() : itemstack, optional.get().getCookingTime())) {
-//                pPlayer.awardStat(Stats.INTERACT_WITH_CAMPFIRE);
-//                return InteractionResult.SUCCESS;
-//            }
-//
-//            return InteractionResult.CONSUME;
-//        }
-
         return InteractionResult.SUCCESS;
     }
 
@@ -125,21 +119,59 @@ public class BingusBlock extends DirectionalBlock {
      */
     @Override
     public void updateEntityAfterFallOn(BlockGetter pLevel, Entity pEntity) {
-        pEntity.setDeltaMovement(pEntity.getDeltaMovement().multiply(1.0D, 0.0D, 1.0D));
-        pEntity.setDeltaMovement(pEntity.getDeltaMovement().multiply(1.0D, 0.0D, 1.0D));
+        super.updateEntityAfterFallOn(pLevel, pEntity);
+
+
+        BlockPos blockPos = pEntity.blockPosition();
+        BlockPos underBlockPos = blockPos;
+        underBlockPos = underBlockPos.below();
+        Direction dir = pLevel.getBlockState(underBlockPos).getValue(FACING);
+        Vec3i vec3iDir = dir.getNormal();
+
+        Vec3 blockOffset = new Vec3(0, 0, 0);
+
+        if(vec3iDir.getX()<0){
+            blockPos = blockPos.east();
+            blockOffset = new Vec3(0, 0, 0.5f);
+            blockOffset = new Vec3(0, 0, 1f);
+        }
+        if(vec3iDir.getX()>0){
+            blockOffset = new Vec3(0, 0, 0.5f);
+            blockOffset = new Vec3(0, 0, 1f);
+        }
+        if(vec3iDir.getZ()<0){
+            blockPos = blockPos.south();
+            blockOffset = new Vec3(0.5f, 0, 0);
+            blockOffset = new Vec3(1, 0, 0);
+        }
+        if(vec3iDir.getZ()>0){
+            blockOffset = new Vec3(0.5f, 0, 0);
+            blockOffset = new Vec3(1, 0, 0);
+        }
+
+        Snowball snowball = new Snowball(pEntity.level, (LivingEntity) pEntity);
+        snowball.setPos(new Vec3(blockPos.getX() +blockOffset.x(), blockPos.getY()-1, blockPos.getZ() + blockOffset.z()));
+        double d0 = blockPos.getY() - (double)1.1F;
+        double d1 = vec3iDir.getX();
+        double d2 = d0 - snowball.getY();
+        double d3 = vec3iDir.getZ();
+        double d4 = Math.sqrt(d1 * d1 + d3 * d3) * (double)0.2F;
+        snowball.shoot(d1, d2 + d4, d3, 1.6F, 6.0F);
+        pEntity.level.addFreshEntity(snowball);
     }
 
     @Override
     public void tick(BlockState state, ServerLevel worldIn, BlockPos pos, RandomSource rand) {
         super.tick(state, worldIn, pos, rand);
-        if(stateDefinition.any().getValue(EATING) == 1){
-            if(chewingTimer > 0){
-                chewingTimer--;
-            }
-            else{
-                stateDefinition.any().setValue(EATING, 2);
-                chewingTimer = chewingTime;
-            }
+        int eatingValue = state.getValue(EATING);
+        if(eatingValue == 1){
+            worldIn.setBlockAndUpdate(pos, state.setValue(EATING, Integer.valueOf(2)));
+//            if(chewingTimer > 0){
+//                chewingTimer--;
+//            }
+//            else{
+//                chewingTimer = chewingTime;
+//            }
         }
     }
 }
